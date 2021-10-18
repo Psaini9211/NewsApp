fc1ce1a927d20eef2729a90b181e514560b4612f
Psaini9211 <86410039+Psaini9211@users.noreply.github.com>
2021-10-18 17:04:26 +0530

    Create slide.js

---
+++ Componants/slide.js
@@ -0,0 +1,34 @@
+
+
+
+
+function showSlideBar() {
+    return `
+       <div id="slideBar">
+            <div id="content">
+         <div id="logo">
+             <img src="../Image/logotype.png">
+             <p>Join Extra Cruch</p>
+         </div>
+         <div id="list_menu">
+             <ul>
+                <li>Login</li>
+                <li id="Search">Search</li>
+                <li id="Disrupt">Disrupt</li>
+                <li>Startups</li>
+                <li>Video</li>
+                <li>Audio</li>
+                <li>Newsletters</li>
+                <li>Extra Crunch</li>
+                <li>EC-1s</li>
+                <li>Advertise</li>
+                <li>Events</li>
+                <li>More</li>
+             </ul>
+         </div>
+        </div>
+        </div>
+    `
+}
+
+export default showSlideBar
